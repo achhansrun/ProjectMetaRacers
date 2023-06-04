1474c9cc1aa449c38edbf556abe25668347cc733
achhansrun <107165544+achhansrun@users.noreply.github.com>
2023-06-04 12:23:08 -0800

    Initial commit

---
+++ ProjectMetaRacers/Intermediate/Build/Win64/UnrealEditor/Inc/ProjectMetaRacers/UHT/ProjectMetaRacers.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeProjectMetaRacers_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_ProjectMetaRacers;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_ProjectMetaRacers()
+	{
+		if (!Z_Registration_Info_UPackage__Script_ProjectMetaRacers.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/ProjectMetaRacers",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x69878AC8,
+				0x8D3ED722,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ProjectMetaRacers.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_ProjectMetaRacers.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ProjectMetaRacers(Z_Construct_UPackage__Script_ProjectMetaRacers, TEXT("/Script/ProjectMetaRacers"), Z_Registration_Info_UPackage__Script_ProjectMetaRacers, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x69878AC8, 0x8D3ED722));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
